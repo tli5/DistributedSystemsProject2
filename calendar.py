259177bdfb57d2e30b8096151cd7f71517f21eec
Greg Weil <weilg@rpi.edu>
2015-10-11 21:12:10 -0400

    A basic calendar event object

--- calendar.py
+++ calendar.py
@@ -1,5 +1,5 @@
-# calendar.py
 #A distributed calendar system
+
 #Distributed Systems Project 1
 #Greg Weil
 #Taoran Li
@@ -7,4 +7,24 @@
 #Run with python 2.7.10
 #No arguments right now
 
-print( "Hello world!" )
+class CalendarEvent:
+	def __init__(self, name, day, start, end):
+		self.name = name
+		self.day = day
+		self.start = start
+		self.end = end
+	
+	def checkConflict(self, other):
+		"""Check if this event conflicts with another"""
+		if (self.day != other.day):
+			return False
+		if (self.start >= other.end):
+			return False
+		if (self.end <= other.start):
+			return False
+		return True
+
+ev1 = CalendarEvent("Event1", 1, 24, 28)
+ev2 = CalendarEvent("Event2", 1, 28, 30)
+
+print(ev1.checkConflict(ev2))
