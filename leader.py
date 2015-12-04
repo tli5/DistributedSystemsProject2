c4c9ca56fcacff484d34aa2313c7faac90854ccf
Greg Weil <weilg@rpi.edu>
2015-12-04 17:04:04 -0500

    A bunch more changes, but it still isn't fixing the issue, I'm going to probably rework most of this

--- leader.py
+++ leader.py
@@ -4,6 +4,7 @@ import network
 
 import socket
 import threading
+import time
 import errno
 
 class LeaderNetwork(object):
@@ -24,26 +25,12 @@ class LeaderNetwork(object):
 	def tcpListen(self):
 		"""Listen for new connections from other processes"""
 		"""This is run on a daemon thread"""
+		poll = time.clock()
 		#Just connect to everything for now
-		self.tcpSend(None, range(len(self.network.peer)))
+		self.tcpSend("hoopla", range(len(self.network.peer)))
 		while True:
 			#Accept connections from other nodes
-			try:
-				con = self.server.accept()
-				node = int(self.tcpReceive(con[0], True))
-				if self.socket[node] and node < self.network.node:
-					self.socket[node].close()
-					self.socket[node] = None
-				if not self.socket[node]:
-					self.socket[node] = con[0]
-					print('connect from ' + str(node))
-				else:
-					print('ignored connect from ' + str(node))
-			except socket.error as e:
-				#No connections to accept
-				if e.errno != errno.EWOULDBLOCK:
-					print('accept', e)
-				pass
+			while self.tcpAccept(): pass
 			#Check for incoming messages
 			for i, sock in enumerate(self.socket):
 				if not sock: continue
@@ -56,8 +43,35 @@ class LeaderNetwork(object):
 						print('disconnect', i)
 						self.socket[i] = None
 						sock.close()
-			self.tcpSend('asdf', range(len(self.network.peer)))
-		
+			#Confirm the connection is alive
+			if time.clock() > poll:
+				print('ping')
+				for i, sock in enumerate(self.socket):
+					if not sock: continue
+					self.tcpSend('', [i])
+				poll = (time.clock() + 5)
+	
+	def tcpAccept(self):
+		"""Accept an incoming connection"""
+		"""This should only be called from the listen thread"""
+		try:
+			con = self.server.accept()
+			node = int(self.tcpReceive(con[0], True))
+			if self.socket[node] and node < self.network.node:
+				self.socket[node].close()
+				self.socket[node] = None
+			if not self.socket[node]:
+				self.socket[node] = con[0]
+				print('connect from ' + str(node))
+			else:
+				con[0].close()
+				print('ignored connect from ' + str(node))
+		except socket.error as e:
+			#No connections to accept
+			if e.errno != errno.EWOULDBLOCK:
+				print('accept', e)
+			return False
+		return True
 	
 	def tcpSend(self, message, targets):
 		"""Send a message to the set of targets"""
@@ -77,16 +91,15 @@ class LeaderNetwork(object):
 				print('con', i, e)
 				pass
 		#Send the message
-		if message:
+		if message != None:
 			for i in targets:
 				if i == self.network.node: continue
 				if not self.socket[i]: continue
 				try:
-					self.socket[i].setblocking(True)
+					self.socket[i].settimeout(5.0)
 					self.socket[i].sendall(message+'\n')
 				except socket.error as e:
 					print('send', i, e)
-			print('sent ' + message)
 	
 	def tcpReceive(self, sock, block = False):
 		"""Receive a message on a tcp socket"""
@@ -94,9 +107,9 @@ class LeaderNetwork(object):
 		"""This should only be called from the listen thread"""
 		msg = ''
 		try:
-			sock.setblocking(block)
+			sock.settimeout(5.0 if block else 0.0)
 			c = sock.recv(1)
-			sock.setblocking(True)
+			sock.settimeout(1.0)
 			while c != '\n':
 				msg += c
 				c = sock.recv(1)
@@ -105,6 +118,8 @@ class LeaderNetwork(object):
 				msg = None
 			else:
 				raise
+		except socket.timeout:
+			msg = None
 		return msg
 	
 	def registerReceive(self, func):
